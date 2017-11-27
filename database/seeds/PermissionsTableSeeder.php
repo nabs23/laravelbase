53f15f74d533c37ef89bf2a21e60d4d511124098
Nabel Odal <nabelodal@gmail.com>
2017-11-27 14:01:45 +0800

    Finished tutorial from https://scotch.io/tutorials/user-authorization-in-laravel-54-with-spatie-laravel-permission by Caleb Oki

---
+++ database/seeds/PermissionsTableSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class PermissionsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $data = [
+            ['name' => 'Edit Post'],
+            ['name' => 'Create Post'],
+        ];
+    }
+}
